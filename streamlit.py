599d46c6cf65f5dcaa2c9c2a18e9a054842de744
Michael Jennings <michaeljenningsai@gmail.com>
2022-07-08 22:10:30 +1000

    eod

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 # %%
+from pickle import FALSE
 import streamlit as st
-import plotly as px
+import plotly.express as px
 import seaborn as sns
 import xgboost as xgb
 import pandas as pd
@@ -11,22 +12,65 @@ from datetime import date,datetime,time,timedelta
 
 st.title('Personal Finance')
 
+
+
 status = pd.read_excel (r'E:/Administration/Life.xlsx', sheet_name='Status')
 expenses = pd.read_excel (r'E:/Administration/Life.xlsx', sheet_name='Expenses')
 today=date.today()
 todayformatted = datetime.today().strftime('%Y-%m-%d')
 
-mortgagerate=st.sidebar.slider('Insert a Mortgage Rate', min_value=0.0, max_value=0.15, value=0.0449, step=0.001)
-proratahours=st.sidebar.slider('Insert Hours Worked', min_value=0, max_value=38, value=32)
 
-carloanrate=0.0679
-ftehours=38
-prorata=proratahours/ftehours
+#Variables
+mortgagerate=st.sidebar.slider('Mortgage Rate %', min_value=0.0, max_value=15.0, value=4.49, step=0.01)
+proratahours=st.sidebar.slider('Pro-rata Hours', min_value=0, max_value=38, value=32)
+
 
 housevalue = 600000
 carvalue = 65400
 furniture = 70000
 super = 210862.21
+odometer = 22500
+fuelprice=2.3
+fuelusage=10.2
+tyrecost = 1800     # $for all 4 replaced
+insurancecar = 12*137.76
+rego = 837.09
+unifreq = 2 # how many times a week i go to uni
+rentalincome = 52*625
+mortgagemin = 26 * 1028.04
+carloanmin = 12 * 903.39
+insurancebuilding = 12*128.6
+insurancecontents = 12*42.74
+insurancelandlord = 232.84
+insuracehealth = 12*137.46
+rateszucc = 4*434
+waterzucc = 1547
+rent = 52*200
+telstra = 12*166
+power = 4*400
+gym = 0
+unifees =  1720.14 
+maintenance = 615
+wfhdays = 199
+extradeduction=1000
+
+#Fixed Values
+carloanrate=0.0679
+ftehours=38
+tyrefreq = 30000    # in Kms
+businesscarcap = 60733 # the 2022 cap for car owned for business
+unidistance = 58.8 # distance to uni
+haircuts = 52/6*32
+rateswang = 4*203
+waterwang = 4*200
+rentaladmin = 12*15*1.1
+advertising = 220
+capitalworks = 9420
+capitalallowances = 1631
+
+#Caluculations
+mortgagerate=mortgagerate/100
+prorata=proratahours/ftehours
 
 status = status[['Date', 'Account 1', 'Account 2',
        'Account 3', 'Account 4', 'Crypto', 'Shares Account', 'Shares',
@@ -54,27 +98,16 @@ debtvalue = mortgagecurrent+carloancurrent
 #Current Networth
 networth=assetvalue-debtvalue
 
-#Car Cost Caluculations
-odometer = 22500
 carpurchase = date(2020,9,30)
 cartimeowned = (today-carpurchase).days/365
 kmperyr = (odometer-335)/cartimeowned
-fuelprice=2.3
-fuelusage=10.2
 fuelperyr = kmperyr/100*fuelusage # fuel used each year
 fuelcostpa = fuelperyr*fuelprice # cost of fuel each year
 carservmean = pd.DataFrame(data={336,552,457,776,378,714,885}).mean()
 carservpa = carservmean*(kmperyr/10000) # $ of servicing per year
-tyrecost = 1800     # $for all 4 replaced
-tyrefreq = 30000    # in Kms
 tyrepa = tyrecost*(kmperyr/30000)
-insurancecar = 12*137.76
-rego = 837.09
 carpa = fuelcostpa+carservpa+tyrepa+insurancecar+rego
 carthousandkm = carpa/kmperyr*1000
-businesscarcap = 60733 # the 2022 cap for car owned for business
-unidistance = 58.8 # distance to uni
-unifreq = 2 # how many times a week i go to uni
 unikms = unidistance * unifreq * 26 # 26 weeks in 2 semesters
 unicarperc = unikms/kmperyr # percentage use of car for uni
 unicarrc = unicarperc * carpa # car running cost for uni
@@ -88,30 +121,11 @@ bonus = 0.07*pretaxsalary*prorata
 salary = pretaxsalary-(((pretaxsalary)-45000)*0.325+5092)
 pension = 26*6.4
 aftertaxincome = salary+pension+salary*0.07
-rentalincome = 52*625
 
 #Expenses
-mortgagemin = 26 * 1028.04
-carloanmin = 12 * 903.39
-insurancebuilding = 12*128.6
-insurancecontents = 12*42.74
-insurancelandlord = 232.84
-insuracehealth = 12*137.46
-rateszucc = 4*434
-waterzucc = 1547
-rent = 52*200
-telstra = 12*166
-power = 4*400
-gym = 0
-haircuts = 52/6*32
-rateswang = 4*203
-waterwang = 4*200
-unifees =  1720.14 
+
 rentalmanagement = (rentalincome*0.09)*1.1
 rentalannual = rentalincome*1.1/52
-rentaladmin = 12*15*1.1
-advertising = 220
-maintenance = 615
 
 expenses = expenses[['Date', 'Invest Prop.', 'Shopping', 'Home', 'Groceries',
        'Transport', 'Utilities', 'Health', 'Eating Out', 'Entertainment',
@@ -124,15 +138,9 @@ lifeexpenses = lifeexpenses[['Groceries','Eating Out','Entertainment','Cash','Fe
 lifeexpenses = lifeexpenses.mean().sum()*12
 
 #Tax stuff
-capitalworks = 9420
-capitalallowances = 1631 
-
 businessinternet = 0.5 * telstra
-wfhdays = 199
 wfhfixedrate = 0.52 * wfhdays
 wfhcost = businessinternet+wfhfixedrate
-
-extradeduction=1000
 
 #Tax Calculations
 mortgageinterest = mortgagecurrent*mortgagerate
@@ -147,17 +155,27 @@ inpocketincome = aftertaxincome+rentalinpocket+taxreturn
 lifeexpenses = mortgagemin+carloanmin+insurancebuilding+insurancecontents+insurancelandlord+insuracehealth+rateszucc+waterzucc+rent+telstra+power+gym+haircuts+rateswang+waterwang+unifees+lifeexpenses+carpa
 savingpotential=inpocketincome-lifeexpenses
 
-st.text(int(totaldeductions))
-st.text(int(totalincome))
-st.text(int(taxableincome))
-st.text(int(taxowed))
-st.text(int(taxpaid))
-st.text(int(taxreturn))
-st.text(int(inpocketincome))
+def bargraph(input):
+       
+       return
 
-st.text("Saving Potential")
-st.text(int(savingpotential))
+col1, col2 = st.columns(2)
+data = {'totalincome':[int(totalincome)],'taxableincome':[int(taxableincome)],'taxreturn':[int(taxreturn)],'inpocketincome':[int(inpocketincome)],'savingpotential':[int(savingpotential)],'totaldeductions':[int(totaldeductions)],'taxowed':[int(taxowed)],'taxpaid':[int(taxpaid)]}
+input = pd.DataFrame(data)
+fig = px.bar(data)
+fig.update_layout(yaxis_range=[0,700000])
+st.plotly_chart(fig, use_container_width=True)
 
+with col1:
+       st.markdown('<b><p style="color:green;font-size:25px;text-align:center;">Total Income: $'+str(int(totalincome))+'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:green;font-size:25px;text-align:center;">Taxable Income: $'+str(int(taxableincome))+'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:green;font-size:25px;text-align:center;">Tax Return: $'+str(int(taxreturn))+'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:green;font-size:25px;text-align:center;">Inpocket Income: $'+str(int(inpocketincome))+'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:blue;font-size:25px;text-align:center;">Saving Potential: $'+ str(int(savingpotential))+'</p></b>', unsafe_allow_html=True)
 
+with col2:
+       st.markdown('<b><p style="color:red;font-size:25px;text-align:center;">Total Deductions: $'+str(int(totaldeductions))+'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:red;font-size:25px;text-align:center;">Tax Owed: $' + str(int(taxowed)) +'</p></b>', unsafe_allow_html=True)
+       st.markdown('<b><p style="color:red;font-size:25px;text-align:center;">Tax Paid: $' + str(int(taxpaid)) + '</p></b>', unsafe_allow_html=True)
 
-
+st.dataframe(data)
